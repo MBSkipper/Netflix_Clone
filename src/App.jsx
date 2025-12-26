fa4bf0fc4d9c1ca50e6e2923494e287e280c9e83
MBSkipper <trawler22@gmail.com>
2025-12-26 15:56:12 +0000

    navbar component added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import NavbarComponent from './components/NavbarComponent'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+       <NavbarComponent />
+    </>
+  )
+}
+
+export default App
+ 
---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import netflixLogo from '../assets/netflix.png'
+
+function NavbarComponent() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container>
+
+        <Navbar.Brand href="#home">
+          <img
+            src={netflixLogo}
+            height="100"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+        
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Form.Select>
+              <option value="English">English</option>
+              <option value="French">French</option>
+              <option value="Spanish">Spanish</option>
+            </Form.Select>
+
+            <Button variant="danger">Sign In</Button>
+
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
